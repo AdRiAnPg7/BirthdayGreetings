99b2e4cf97e0ece47667d5b99601ca7e2f06e7bc
Adrian <adriancracker@gmail.com>
2020-05-11 10:18:31 -0400

    Refactorizacion

---
+++ birthday-greetings/src/main/java/it/xpug/birthday_greetings/Main.java
@@ -0,0 +1,24 @@
+package it.xpug.birthday_greetings;
+
+import java.io.*;
+import java.text.ParseException;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class Main {
+
+	public static void main(String[] args) throws AddressException, IOException, ParseException, MessagingException {
+		IEmployeeRepository repository = new FileEmployeeRepository("employee_data.txt");
+		IEmailService emailService = new SmtpMailService("localhost", 25);
+	
+		BirthdayService service = new BirthdayService(repository, emailService);
+		service.sendGreetings(new XDate());
+	
+		/*BirthdayService service = new BirthdayService();
+		service.sendGreetings("employee_data.txt", new XDate(), "localhost", 25);
+		 */	
+	}
+
+}
+ 
